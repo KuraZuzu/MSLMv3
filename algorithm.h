8a9ace1064f1af48487dc29a7890f70d3360e0bb
DESKTOP-NJGKPPP\yamany <yamany7@live.jp>
2018-11-25 22:20:07 +0900

    algorithm体操第一！

---
+++ algorithm.h
@@ -0,0 +1,14 @@
+//
+// Created by yamany on 2018/11/25.
+//
+
+#ifndef MSLMV3_ALGORITHM_H
+#define MSLMV3_ALGORITHM_H
+
+#include "Map3.h"
+
+void saitan(Map3 map){
+
+}
+
+#endif //MSLMV3_ALGORITHM_H
