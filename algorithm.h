2020092b16846e9c312a7cf11182a0a851966d3f
DESKTOP-NJGKPPP\yamany <yamany7@live.jp>
2018-11-28 22:30:04 +0900

    昨日の分コミット未完成

--- algorithm.h
+++ algorithm.h
@@ -10,22 +10,20 @@
 void saitan(Map3 &map){
     int tmp_cnt = 0;
     Point<uint8_t > v_point;
-    v_point = map.size();
     Map3 walk_map = walk_map;
     Block tmp_block;
     bool change_flg=false;
-
+    v_point.x=(v_point.x-1)/2;
+    v_point.y=(v_point.y-1)/2;
+    tmp_block.walk_cnt=0;
+    walk_map.set_block(tmp_block, v_point);
     do {
         change_flg = false;
-        for(uint8_t i=1; i<map.size().x-1; i++){
-            for(uint8_t j=1; j<map.size().y-1; j++){
-                v_point.x=i;
-                v_point.y=j;
+        for(uint8_t i=0; i<map.size().x-1; i++){
+            for(uint8_t j=0; j<map.size().y-1; j++){
                 tmp_block = map.at(v_point);
-
-                if(walk_map.at(v_point).walk_cnt==255)
+                if(walk_map.at(x,y)==255)
                     continue;
-                if(j < v_point.y-1){
                     if((tmp_block.get_wall() & NORTH_MASK) != NORTH_MASK){
                         tmp_cnt = tmp_block.walk_cnt;
                         v_point.y++;
@@ -36,8 +34,6 @@ void saitan(Map3 &map){
                             change_flg = true;
                         }
                     }
-                }
-                if(i < v_point.x-1){
                     if((tmp_block.get_wall() & EAST_MASK) != EAST_MASK){
                         tmp_cnt = tmp_block.walk_cnt;
                         v_point.x++;
@@ -48,8 +44,6 @@ void saitan(Map3 &map){
                             change_flg = true;
                         }
                     }
-                }
-                if(j>1){
                     if((tmp_block.get_wall() & SOUTH_MASK) != SOUTH_MASK){
                         tmp_cnt = tmp_block.walk_cnt;
                         v_point.y--;
@@ -60,8 +54,6 @@ void saitan(Map3 &map){
                             change_flg = true;
                         }
                     }
-                }
-                if(i>1){
                     if((tmp_block.get_wall() & WEST_MASK) != WEST_MASK){
                         tmp_cnt = tmp_block.walk_cnt;
                         v_point.x--;
@@ -72,11 +64,10 @@ void saitan(Map3 &map){
                             change_flg = true;
                         }
                     }
-                }
             }
         }
 
-    }while(change_flg == true);
+    }while(change_flg);
 
 
 }
